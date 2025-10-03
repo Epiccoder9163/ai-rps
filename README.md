a5cd5c0cb313ad8f944e97ad79851d562d664e41
elliottwise <pikachuman20@outlook.com>
2025-10-03 16:09:33 -0400

    Added initial files
    Added the initial files

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# ai-rps
- AI based rock paper scissors game
+# AI-RPS
+ A very basic AI based rock paper scissors game
+Uses any LLM of your choice, but I have found that qwen3:0.6b works the best.
+
+## Requirements
+A desktop computer
+Ollama installed and running
+Python 3
+## How to install
+Clone the repository
+cd into the respository folder
+create a venv environment (python3 -m venv venv)
+Activate the venv environment (source venv/bin/activate)
+Install the prerequisites (pip install -r requirements.txt)
+Run the script (python3 ./rockpaperscissors.py)
---
+++ rockpaperscissors.py
@@ -0,0 +1,197 @@
+import ollama
+import os
+import time
+import re
+from langchain.chat_models import ChatOllama
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema import HumanMessage
+from langchain_core.messages import HumanMessage
+from langchain_core.output_parsers import StrOutputParser
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationChain
+
+# The model used for the internal large language model
+# Preferably use a smaller model, as it will work better on weaker or older hardware, specifically that without a dGPU
+model = "qwen3:0.6b"
+
+# The prompt for the internal large language model used as your opponent in rock paper scissors
+prompt = """
+You are an AI designed solely to play rock paper scissors. You will chose either rock, paper, or scissors. You will 
+ONLY output either rock, paper, or scissors, depending on what you chose, with no feedback or other. DO NOT PRODUCE ANY OTHER OUTPUT OTHER THAN 
+EITHER rock, paper, or scissors. Don't overthink this prompt, just pick logically. Make sure to pick a different result each time. Be sure to simply pick randomly.
+"""
+
+# Initialize the internal variables
+previous = []
+ai_prev = []
+human_prev = []
+ai_current = ""
+human_current = ""
+human_wins = 0
+ai_wins = 0
+
+
+# Check if the models listed above are available
+if model in str(ollama.list()):
+    print("The selected model is available!")
+else:
+    print("The selected model is not available!")
+    print("Downloading now")
+    ollama.pull(model)
+os.system('clear')
+
+# Initialize the LLM
+llm = ChatOllama(base_url="http://localhost:11434", model=model, reasoning=False)
+memory = ConversationBufferMemory()
+conversation = ConversationChain(llm=llm, memory=memory)
+
+
+def human(human_temp, ai_temp):
+    global human_current
+    global ai_current
+    os.system('clear')
+    print("Your previous responses: " + human_temp)
+    print("Your opponent's previous responses: " + ai_temp)
+    print(" ")
+    while True:
+        response = str(input("What move do you make? (scissors/paper/rock): "))
+        if response == 'scissors' or response == 'paper' or response == 'rock':
+            break
+        else:
+            os.system('clear')
+            print("This response is invalid!")
+            time.sleep(2)
+            os.system('clear')
+    human_current = response
+    return
+
+def ai(human_temp, ai_temp):
+    global human_current
+    global ai_current
+    os.system('clear')
+    context_prompt = ChatPromptTemplate.from_messages([
+        ("system", prompt),
+        ("human", human_temp + ai_temp)
+    ])
+    chain = context_prompt | llm
+    response = str(chain.invoke({"context": context_prompt, "main_prompt": prompt}).content)
+    response = re.sub(r"<think>[\s\S]*?</think>\n*\n*", "", response)
+    # Find all words in the string
+    words = re.findall(r'\b\w+\b', response)
+    # Return the last one if it exists
+    response = words[-1] if words else ""
+    ai_current = response
+    print(ai_current)
+    time.sleep(2)
+    return
+
+def match():
+    global human_current
+    global ai_current
+    global ai_wins
+    global human_wins
+    os.system('clear')
+    # This section checks what answers were picked
+    if human_current == "paper" and ai_current == "rock":
+        print("Paper covers rock!")
+        print("Human wins!")
+        human_wins += 1
+    elif human_current == "scissors" and ai_current == "paper":
+        print("Scissors cuts paper!")
+        print("Human wins!")
+        human_wins += 1
+    elif human_current == "rock" and ai_current == "scissors":
+        print("Rock crushes scissors!")
+        print("Human wins!")
+        human_wins += 1
+    elif human_current == "paper" and ai_current == "scissors":
+        print("Scissors cuts paper!")
+        print("AI wins!")
+        ai_wins += 1
+    elif human_current == "rock" and ai_current == "paper":
+        print("Paper covers rock!")
+        print("AI wins!")
+        ai_wins += 1
+    elif human_current == "scissors" and ai_current == "rock":
+        print("Rock crushes scissors!")
+        print("AI wins!")
+        ai_wins += 1
+    elif human_current == ai_current:
+        print("Tie!")
+        ai_wins += 1
+        human_wins += 1
+    else:
+        print("The large language model has produced an invalid response! Reattempting")
+        ai(human_temp, ai_temp)
+        match()
+        return
+    print("The AI now has " + str(ai_wins) + " wins")
+    print("You now have " + str(human_wins) + " wins")
+    time.sleep(4)
+    human_prev.append(human_current)
+    ai_prev.append(ai_current)
+    return
+while True:
+    os.system('clear')
+    repeat_count = input("How many games would you like to play?: ")
+    if repeat_count.isdigit() == True:
+        repeat_count = int(repeat_count)
+        break
+    else:
+        os.system('clear')
+        print("This is an invalid input!")
+        time.sleep(2)
+        os.system('clear')
+# Main game loop
+for i in range(0, repeat_count):
+    human_temp = ''
+    ai_temp = ''
+    for i in range(0, len(human_prev)):
+        human_temp += str(human_prev[i]) + ", "
+        ai_temp += str(ai_prev[i]) + ", "
+    human(human_temp, ai_temp)
+    ai(human_temp, ai_temp)
+    match()
+while True:
+        os.system('clear')
+        end_game = input("Would you like to play again? (True/False): ")
+        if end_game == 'true' or end_game == 'True' or end_game == 't' or end_game == 'T':
+            break
+        elif end_game == 'false' or end_game == 'False' or end_game == 'f' or end_game == 'F':
+            os.system('clear')
+            print("Final Results: ")
+            print(" ")
+            print("AI wins: " + str(ai_wins))
+            print("Your wins: " + str(human_wins))
+            exit()
+        else:
+            os.system('clear')
+            print("This is an invalid input!")
+            time.sleep(2)
+            os.system('clear')
+while True:
+    human_temp = ''
+    ai_temp = ''
+    for i in range(0, len(human_prev)):
+        human_temp += str(human_prev[i]) + ", "
+        ai_temp += str(ai_prev[i]) + ", "
+    human(human_temp, ai_temp)
+    ai(human_temp, ai_temp)
+    match()
+    os.system('clear')
+    while True:
+        end_game = input("Would you like to play again? (True/False): ")
+        if end_game == 'true' or end_game == 'True' or end_game == 't' or end_game == 'T':
+            break
+        elif end_game == 'false' or end_game == 'False' or end_game == 'f' or end_game == 'F':
+            os.system('clear')
+            print("Final Results: ")
+            print(" ")
+            print("AI wins: " + str(ai_wins))
+            print("Your wins: " + str(human_wins))
+            exit()
+        else:
+           os.system('clear')
+           print("This is an invalid input!")
+           time.sleep(2)
+           os.system('clear')
